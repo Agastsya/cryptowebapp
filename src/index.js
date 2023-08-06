027aad17dbebde1b554a2a063bd4fe1ac38180bd
agastsya <agastsya007@gmail.com>
2023-08-07 01:56:37 +0530

    color theme added

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
+import ColorModeSwitcher from "./ColorModeSwitcher";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
+      <ColorModeSwitcher />
       <App />
     </ChakraProvider>
   </React.StrictMode>
